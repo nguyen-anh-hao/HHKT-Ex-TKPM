9f455e07888c4524f5c86965fdd0d53ef6b7b9b0
eNKay <nguyenkhai24082408@gmail.com>
2025-03-21 20:42:27 +0700

    feat: add findByName method to check duplicate

--- server/src/main/java/org/example/backend/repository/ITinhTrangRepository.java
+++ server/src/main/java/org/example/backend/repository/ITinhTrangRepository.java
@@ -4,6 +4,9 @@ import org.example.backend.domain.TinhTrangSinhVien;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface ITinhTrangRepository extends JpaRepository<TinhTrangSinhVien, Integer> {
+    Optional<TinhTrangSinhVien> findByTenTinhTrang(String tenTinhTrang);
 }
