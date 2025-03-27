7738d6f0da3bcdbbb2325c693a9aaa36d6dd547f
BrianTrac <dangphuchung321@gmail.com>
2025-03-27 20:10:31 +0700

    feat(student-status-rules): Add mapper for student status rules

--- server/src/main/java/org/example/backend/mapper/StudentStatusRuleMapper.java
+++ server/src/main/java/org/example/backend/mapper/StudentStatusRuleMapper.java
@@ -1,4 +1,28 @@
 package org.example.backend.mapper;
 
+import org.example.backend.domain.StudentStatus;
+import org.example.backend.domain.StudentStatusRule;
+import org.example.backend.dto.request.StudentStatusRuleRequest;
+import org.example.backend.dto.response.StudentStatusRuleResponse;
+
 public class StudentStatusRuleMapper {
+
+    public static StudentStatusRule mapFromRequestToDomain(StudentStatus currentStatus, StudentStatus allowedTransition) {
+        return StudentStatusRule.builder()
+                .currentStatus(currentStatus)
+                .allowedTransition(allowedTransition)
+                .build();
+    }
+
+    public static StudentStatusRuleResponse mapFromDomainToStudentStatusRuleResponse(StudentStatusRule studentStatusRule) {
+        return StudentStatusRuleResponse.builder()
+                .id(studentStatusRule.getId())
+                .currentStatusName(studentStatusRule.getCurrentStatus().getStudentStatusName())
+                .allowedTransitionName(studentStatusRule.getAllowedTransition().getStudentStatusName())
+                .createdAt(studentStatusRule.getCreatedAt())
+                .updatedAt(studentStatusRule.getUpdatedAt())
+                .createdBy(studentStatusRule.getCreatedBy())
+                .updatedBy(studentStatusRule.getUpdatedBy())
+                .build();
+    }
 }
