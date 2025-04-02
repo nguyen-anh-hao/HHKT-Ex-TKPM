d52de310362b3eb705c8335c3f0ca85764d1300b
BrianTrac <dangphuchung321@gmail.com>
2025-04-02 17:37:24 +0700

    feat(Class Registration): Add logic CRUD for Class Registration class

--- server/src/main/java/org/example/backend/dto/response/ClassRegistrationResponse.java
+++ server/src/main/java/org/example/backend/dto/response/ClassRegistrationResponse.java
@@ -1,4 +1,28 @@
 package org.example.backend.dto.response;
 
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@Builder
 public class ClassRegistrationResponse {
+    private Integer id;
+    private String status;
+    private LocalDateTime registrationDate;
+    private LocalDateTime cancellationDate;
+
+    private String studentId;
+    private String studentName;
+
+    private Integer classId;
+    private String classCode;
+
+    private LocalDateTime createdDate;
+    private LocalDateTime updatedDate;
+    private String createdBy;
+    private String updatedBy;
 }
--- server/src/main/java/org/example/backend/service/IClassRegistrationService.java
+++ server/src/main/java/org/example/backend/service/IClassRegistrationService.java
@@ -1,4 +1,14 @@
 package org.example.backend.service;
 
+import org.example.backend.dto.request.ClassRegistrationRequest;
+import org.example.backend.dto.response.ClassRegistrationResponse;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
 public interface IClassRegistrationService {
+    Page<ClassRegistrationResponse> getAllClassRegistrations(Pageable pageable);
+
+    ClassRegistrationResponse getClassRegistrationById(Integer id);
+
+    ClassRegistrationResponse addClassRegistration(ClassRegistrationRequest classRegistrationRequest);
 }
