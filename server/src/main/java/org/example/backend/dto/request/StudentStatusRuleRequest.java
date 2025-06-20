56f4d44f217eb9c76ca08efce282c63a26c18deb
BrianTrac <dangphuchung321@gmail.com>
2025-06-20 21:46:47 +0700

    test: add 8 test cases for course controller test

--- server/src/main/java/org/example/backend/dto/request/StudentStatusRuleRequest.java
+++ server/src/main/java/org/example/backend/dto/request/StudentStatusRuleRequest.java
@@ -2,11 +2,13 @@ package org.example.backend.dto.request;
 
 import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotNull;
-import lombok.Getter;
-import lombok.Setter;
+import lombok.*;
 
 @Getter
 @Setter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class StudentStatusRuleRequest {
 
     @NotNull(message = "Student status rule id is required")
